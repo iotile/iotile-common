12deb7a7ce677db886665689bd6c89230c9a63c6
Tim Burke <timburke@alumni.rice.edu>
2019-01-02 10:23:17 -0800

    Update logging base

--- src/classes/all-base.ts
+++ src/classes/all-base.ts
@@ -1,35 +1,47 @@
 /**
- * Legacy base object that assumes we are using angular logging
+ * Legacy base object that previously assumed we were using angular logging
  */
-export class ObjectBase {
-    protected $injector: any;
-    protected $log: any;
 
+import {LoggingBase} from "./logging-base";
+import {Category} from "typescript-logging";
+
+export class ObjectBase extends LoggingBase {
+    protected $injector: any;
     private className: string;
 
+    static categoryMap: {[key: string]: Category} = {};
+    static categoryBase = new Category("object");
+
+    protected static createOrGetCategory(name: string): Category {
+        if (!(name in ObjectBase.categoryMap))
+            this.categoryMap[name] = new Category(name, ObjectBase.categoryBase);
+        
+        return this.categoryMap[name];
+    }
     constructor(name: string, $injector: any) {
+        super(ObjectBase.createOrGetCategory(name));
+
         this.className = name;
-        this.$log = $injector.get("$log");
         this.$injector = $injector;
     }
 
     protected log_debug(message: string, ...args: any[])
     {
-        this.$log.debug("[" + this.className + "] " + message, ...args);
+        this.logDebug(message, args);
     }
 
     protected log_warn(message: string, ...args: any[])
     {
-        this.$log.warn("[" + this.className + "] " + message, ...args);
+        this.logWarning(message, args);
     }
 
     protected log_error(message: string, ...args: any[])
     {
-        this.$log.error("[" + this.className + "] " + message, ...args);
+        this.logError(message, args);
     }
 
     protected log_info(message: string, ...args: any[])
     {
-        this.$log.info("[" + this.className + "] " + message, ...args);
+        this.logInfo(message, args);
     }
 }
